4fba932c07153b8f71a3e5a0b74cb5960fd47692
Luigi Morel <morelmiles@gmail.com>
2022-01-04 23:15:35 +0300

    delete some files

--- resources/js/components/common/dashboard/container.dashboard.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const DashboardContainer = () => {
-    return <div>this is the main container</div>;
-};
-
-export default DashboardContainer;
--- resources/js/components/common/dashboard/sidebar.dashboard.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const SidebarDashboard = () => {
-    return (
-        <div className=" w-64 bg-blue-400">this is the sidebar dashboard</div>
-    );
-};
-
-export default SidebarDashboard;
