e87d9d68925903fb08d53f886f4ef474f8a95a55
Luigi Morel <morelmiles@gmail.com>
2022-01-04 23:15:50 +0300

    add hero section

--- resources/js/components/common/Hero.tsx
+++ resources/js/components/common/Hero.tsx
@@ -1,33 +1,42 @@
-import React from "react";
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 // Assets
-import HeroImg from "./../../../assets/images/hero.jpg";
+import HeroImg from './../../../assets/images/hero.jpg'
 
 // Components
-import Button from "./Button";
+import Button from './Button'
 
 const Hero = () => {
-    return (
-        <>
-            <div className=" flex flex-row justify-between h-screen w-full sm:px-24 tab:px-12 px-3 ">
-                <div className=" w-1/2 mt-24 sm:mr-12">
-                    <h3 className=" text-5xl mb-8 uppercase font-sans">
-                        Revolutionizing monitoring in the construction industry.
-                    </h3>
+	return (
+		<>
+			<div className=" flex flex-row justify-between h-screen w-full sm:px-24 tab:px-12 px-3 ">
+				<div className=" w-1/2 mt-24 sm:mr-12">
+					<h3 className=" text-5xl mb-8 uppercase font-sans">
+						Revolutionizing monitoring in the construction industry.
+					</h3>
 
-                    <p className=" mb-8 text-lg text-gray-700">
-                        Pioneers of the constrution industry monitoring in the
-                        Sub Saharan Africa Region.
-                    </p>
-                    <Button text="Sign Up" className="  " />
-                </div>
+					<p className=" mb-8 text-lg text-gray-700">
+						Pioneers of the constrution industry monitoring in the Sub Saharan Africa
+						Region.
+					</p>
 
-                <div className=" w-1/2 ">
-                    <img src={HeroImg} alt="" className=" h-4/5 w-full" />
-                </div>
-            </div>
-        </>
-    );
-};
+					<p className=" mb-8 text-lg text-gray-700">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis veniam
+						possimus, ipsa at consectetur accusantium? Voluptas aut sed suscipit quod!
+					</p>
 
-export default Hero;
+					<Link to="/solutions">
+						<Button text="Explore More" className="  " />
+					</Link>
+				</div>
+
+				<div className=" w-1/2 ">
+					<img src={HeroImg} alt="" className=" h-4/5 w-full" />
+				</div>
+			</div>
+		</>
+	)
+}
+
+export default Hero
