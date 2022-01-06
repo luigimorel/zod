5d26423c6ac678d9fc74c8c501ee48714e24c523
Luigi Morel <morelmiles@gmail.com>
2022-01-06 10:43:55 +0300

    add settings pages

---
+++ resources/js/components/dashboard/settings/client.settings.tsx
@@ -0,0 +1,137 @@
+import React, { useState } from 'react'
+
+// Assets
+import Logo from './../../../../assets/images/logo.png'
+
+// Components
+import DashNav from '../admin/dashNav'
+import SidebarDashboard from '../clients/sidebar.dashboard'
+import MobileNavbarClientsDashboard from '../clients/mobileNav.clients'
+
+const AdminSettings = () => {
+ const [open, setOpen] = useState(false)
+
+ return (
+  <>
+   <div className=" pb-10 hidden fixed w-full sm:block ">
+    <DashNav />
+   </div>
+   <div className="flex flex-row justify-between sm:hidden   bg-white   shadow-sm   w-full sm:px-24 tab:px-12 px-3  items-center py-6 navbar ">
+    <a href="/">
+     <img src={Logo} alt="" className=" sm:h-10 h-5" />
+    </a>
+    <p onClick={() => setOpen(!open)} className=" sm:hidden">
+     Menu
+    </p>
+
+    {open && <MobileNavbarClientsDashboard />}
+   </div>
+   <div className="flex flex-row w-full pt-4 sm:pt-20">
+    <div className=" w-64 hidden sm:flex ">
+     <SidebarDashboard />
+    </div>
+
+    {/* Start of the form  */}
+    <div className=" sm:w-1/3   mx-auto flex justify-center flex-col">
+     <p className=" mb-5 text-gray-800 text-lg mt-4 text-center font-bold">
+      Account Details' Change
+     </p>
+     <form action="" method="post">
+      <div className="flex flex-col">
+       <label htmlFor="firstName" className=" mb-2.5">
+        First Name
+       </label>
+       <input
+        type="text"
+        name="firstName"
+        required
+        id="firstName"
+        className="   mb-4 rounded-sm border-gray-400"
+       />
+      </div>
+
+      {/* Last Name */}
+      <div className="flex flex-col">
+       <label htmlFor="lastName" className=" mb-2.5">
+        Last Name
+       </label>
+       <input
+        type="text"
+        name="lastName"
+        required
+        id="lastName"
+        className=" w-full mb-4 rounded-sm border-gray-400"
+       />
+      </div>
+      {/* Company Name  */}
+
+      {/* Last Name */}
+      <div className="flex flex-col">
+       <label htmlFor="companyName" className=" mb-2.5">
+        Company Name
+       </label>
+       <input
+        type="text"
+        required
+        name="companyName"
+        id="companyName"
+        className=" w-full mb-4 rounded-sm border-gray-400"
+       />
+      </div>
+      {/* Email */}
+      <div className="flex flex-col">
+       <label htmlFor="email" className=" mb-2.5">
+        Business Email
+       </label>
+       <input
+        type="email"
+        name="email"
+        required
+        readOnly
+        id="email"
+        className=" w-full mb-4 rounded-sm border-gray-400"
+       />
+      </div>
+      {/* Name */}
+      <div className="flex flex-col">
+       <label htmlFor="tel  " className=" mb-2.5">
+        Business Phone Number
+       </label>
+
+       <input
+        type="tel"
+        name="tel"
+        required
+        id="tel"
+        className=" w-full mb-4 rounded-sm border-gray-400"
+       />
+      </div>
+
+      {/* Change password */}
+      {/* Name */}
+      <div className="flex flex-col">
+       <label htmlFor="tel  " className=" mb-2.5">
+        Change Password
+       </label>
+
+       <input
+        type="tel"
+        name="tel"
+        required
+        id="tel"
+        className=" w-full mb-4 rounded-sm border-gray-400"
+       />
+      </div>
+      <div className=" flex justify-center mb-12">
+       <button className=" bg-sidebar text-center px-8 py-4 text-white ">
+        Save Changes
+       </button>
+      </div>
+     </form>
+    </div>
+   </div>
+  </>
+ )
+}
+
+export default AdminSettings
