ad548ed076457a4a2c91ce33d5ca4563c1df6a6c
Luigi Morel <morelmiles@gmail.com>
2022-01-10 22:21:31 +0300

    change layout

--- resources/js/components/common/Hero.tsx
+++ resources/js/components/common/Hero.tsx
@@ -1,25 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-// Assets
-import HeroImg from './../../../assets/images/hero.jpg'
-
 // Components
 import Button from './Button'
 
 const Hero = () => {
  return (
   <>
-   <div className=" flex sm:-mt-12  sm:flex-row flex-col-reverse items-center   justify-between h-screen w-full sm:mb-10     ">
-    <div className=" sm:w-1/2 w-full   sm:pl-24 tab:pl-12 pl-3  sm:mr-12">
+   <div className=" flex sm:-mt-16 px-4  sm:flex-row tab:mt-32 tab:flex-col-reverse flex-col-reverse items-center relative   justify-between h-screen w-full sm:mb-10     ">
+    <div className=" sm:w-1/2 w-full tab:w-full   sm:pl-24 tab:pl-12 pl-3 tab:mr-0 tab:px-4  sm:mr-12">
      <h3 className=" sm:text-5xl text-3xl mb-8 font-sans font-semibold uppercase  ">
       CONSTRUCTION SITE MONITORING SIMPLIFIED
      </h3>
 
-     <p className=" mb-8 text-lg text-gray-800">
+     <p className=" mb-12 text-lg sm:mr-4 text-gray-800">
       We are committed to helping construction industry investors, developers
-      and project managers easily get their work <br /> done through real time
-      monitoring of their <br /> construction sites in real time.
+      and project managers easily get their work done through real time
+      monitoring of their construction sites.
      </p>
      <div className="flex justify-center sm:justify-start">
       <Link to="/solutions">
@@ -28,13 +25,13 @@ const Hero = () => {
      </div>
     </div>
 
-    <div className=" sm:w-1/2 w-full tab:w-full mr-12 ">
+    <div className=" sm:w-1/2 w-full tab:w-full mb-8 sm:mb-0 sm:border-cta  rounded-3xl tab:rounded-xl tab:border-2 border-8 tab:mr-0 tab:px-8  sm:mr-12 ">
      <img
       src={
        'https://res.cloudinary.com/morelmiles/image/upload/v1641589820/contruction_img_gxsiok.jpg'
       }
       alt=""
-      className=" rounded w-auto h-3/5"
+      className=" w-auto sm:h-3/5 h-full tab:h-full tab:mb-10 rounded-2xl"
      />
     </div>
    </div>
--- resources/js/components/common/IconsSection.tsx
+++ resources/js/components/common/IconsSection.tsx
@@ -17,20 +17,22 @@ const IconsSection = () => {
    id: 1,
    heading: 'Excellence ',
    img: Excellence,
-   text: 'We aim at being excellent at delivering value for our clients.',
+   text:
+    'We aim at being excellent at delivering insightful value and data for our clients.',
   },
   {
    id: 2,
    heading: 'Simplicity ',
    img: SimpleIcon,
    text:
-    'Simplicity is the ultimate sophistication. Denebolatech is a simple tool to use but delivers measurable value.',
+    'We prioritize simplicity over sophistication. A simple tool to use but delivers measurable value.',
   },
   {
    id: 3,
-   heading: 'User Centric ',
+   heading: 'User Centricity ',
    img: User,
-   text: 'Users come first in whatever as we strive to satsify their needs.',
+   text:
+    'We place users at the forefront of whatever we do as we strive to satsify your needs.',
   },
   {
    id: 4,
@@ -44,26 +46,25 @@ const IconsSection = () => {
    heading: 'Disruption ',
    img: Disruption,
    text:
-    'Our goal is to make considerable impact on how the daily operations in the construction industry are carried out. ',
+    'Our goal is to make considerable impact on the daily operations in the construction industry are carried out. ',
   },
   {
    id: 6,
    heading: 'Dependable',
    img: Dependable,
    text:
-    'We are available to help in case of an emergency all the time. Reach out to us.',
+    'We are eager to help in case of an emergency ALL the time. Please reach out to us.',
   },
  ]
  return (
   <div className=" mb-14">
-   <Heading text=" OUR VALUES" />
-   <p className=" mb-8 text-xl sm:w-2/4 w-full sm:px-20 px-4 mx-auto my-0">
-    We have a set of values that guide our way of doing business... And{' '}
-    <b>WE</b> {'  '}
-    are true to whatever we say...
+   <Heading text=" OUR VALUES " className=" font-sans font-bold" />
+   <p className=" text-xl sm:w-2/4 tab:w-2/3 w-full tab:mb-4 sm:px-20 font-sans px-4 mx-auto my-0">
+    We have a set of values that guide our way of doing business. <b>WE{''}</b>{' '}
+    are true to whatever we say we are.
    </p>
 
-   <div className=" grid sm:grid-cols-3 grid-rows-1  sm:px-20 px-4 mb-12">
+   <div className=" grid sm:grid-cols-3 tab:grid-cols-2 grid-rows-1  sm:px-20 px-4 mb-12">
     {iconCards.map((x) => (
      <div
       className="flex flex-col mr-0 py-8 mb-6 px-3 mx-4 hover:border-b-blue-700 hover:bg-gray-100  hover:border-b-2 hover:shadow-md  "
@@ -73,7 +74,7 @@ const IconsSection = () => {
       <h6 className=" text-center text-xl mb-4 sm:mb-0 font-bold text-[#212121]">
        {x.heading}
       </h6>
-      <p className=" sm:py-6 sm:px-8   text-lg">{x.text}</p>
+      <p className=" sm:py-6 sm:px-8 tab:px-0   text-lg">{x.text}</p>
      </div>
     ))}
    </div>
