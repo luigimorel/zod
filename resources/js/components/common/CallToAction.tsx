3f00b6adba0dfde18ce8d3ab229f8472f6d36b6b
Luigi Morel <morelmiles@gmail.com>
2022-01-10 22:21:18 +0300

    change bg

--- resources/js/components/common/CallToAction.tsx
+++ resources/js/components/common/CallToAction.tsx
@@ -5,18 +5,21 @@ import Button from './Button'
 
 const CallToAction = () => {
  return (
-  <div className=" bg-cta">
-   <div className=" sm:px-32 px-8 flex flex-col">
+  <div className=" bg-cta mb-8">
+   <div className=" sm:px-32 px-8 tab:px-16 flex flex-col">
     <p className=" text-xl text-white mb-12 mt-8">
      Built for construction site monitoring
     </p>
 
-    <p className=" text-4xl text-white mb-7">Speed, flexibility and accuracy</p>
+    <p className=" text-4xl text-white mb-7">
+     Speed, flexibility and accuracy...
+    </p>
 
     <p className=" text-white text-base mb-12">
      Make faster and more informed data-driven decisions, stay on top of your
-     project, and keep all stakeholders <br className=" sm:block hidden" />{' '}
-     informed while reducing surprises, delays, cost overruns, and claims.
+     project, and keep all stakeholders{' '}
+     <br className=" sm:block hidden tab:hidden" /> informed while reducing
+     surprises, delays, cost overruns and claims.
     </p>
 
     <div className="mb-4">
