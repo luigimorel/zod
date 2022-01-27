8de3c2e09f285057bc8a2475c6d9c3e2c5708c50
Luigi Morel <morelmiles@gmail.com>
2022-01-28 00:08:18 +0300

    add other routes related to user account managemen

--- resources/js/components/App.tsx
+++ resources/js/components/App.tsx
@@ -1,33 +1,48 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
 
-// Components
+// Static pages
 import Home from './pages/Home'
 import Solutions from './pages/Solutions'
-import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
-import AdminDashboard from './pages/AdminDashboard'
 import Contact from './pages/Contact'
+
+// User account management
 import SignIn from './accounts/SignIn'
 import SignUp from './accounts/SignUp'
+import RecoveryPassword from './accounts/RecoverPassword'
+import RecoveryAccountSuccess from './accounts/RecoveryAccountSuccess'
+import ResetPassword from './accounts/ResetPassword'
+
+// Dashboards
 import ClientDashboard from './pages/ClientDashboard'
 import AdminSettings from './dashboard/settings/admin.settings'
+import AdminDashboard from './pages/AdminDashboard'
 import ClientSettings from './dashboard/settings/client.settings'
 
 function App() {
  return (
-  <Router>
+  <BrowserRouter>
    <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/solutions" element={<Solutions />} />
-    <Route path="/admin-dashboard" element={<AdminDashboard />} />
     <Route path="/contact-us" element={<Contact />} />
+    {/* Components for the account management  */}
+    <Route path="/accounts/recover-account" element={<RecoveryPassword />} />
     <Route path="/accounts/login" element={<SignIn />} />
     <Route path="/accounts/sign-up" element={<SignUp />} />
-    <Route path="client-dashboard" element={<ClientDashboard />} />
+    <Route
+     path="/accounts/recovery-success"
+     element={<RecoveryAccountSuccess />}
+    />
+    <Route path="/accounts/reset-password" element={<ResetPassword />} />
+    {/* Dashboard components  */}
+    <Route path="/client-dashboard" element={<ClientDashboard />} />{' '}
+    <Route path="/admin-dashboard" element={<AdminDashboard />} />
     <Route path="/admin-dashboard/settings" element={<AdminSettings />} />
     <Route path="/client-dashboard/settings" element={<ClientSettings />} />
    </Routes>
-  </Router>
+  </BrowserRouter>
  )
 }
 
