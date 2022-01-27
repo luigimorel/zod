fd5b3fe8a320dd15ca6b37c2db1033311ccacebb
Luigi Morel <morelmiles@gmail.com>
2022-01-28 00:07:43 +0300

    update the design and colors

--- resources/js/components/common/IconsSection.tsx
+++ resources/js/components/common/IconsSection.tsx
@@ -7,13 +7,10 @@ import Heading from './Heading'
 // @ts-ignore
 import SimpleIcon from './../../../assets/icons/Simple.svg'
 // @ts-ignore
-
 import Excellence from './../../../assets/icons/Excellence.svg'
 // @ts-ignore
-
 import Disruption from './../../../assets/icons/Disruption.svg'
 // @ts-ignore
-
 import Loyalty from './../../../assets/icons/Loyalty.svg'
 // @ts-ignore
 import Dependable from './../../../assets/icons/Dependable.svg'
@@ -62,13 +59,14 @@ const IconsSection = () => {
    heading: 'Dependable',
    img: Dependable,
    text:
-    'We are eager to help in case of an emergency ALL the time. Please reach out to us.',
+    'We are eager to help in case of an emergency at ANY time. Please reach out to us.',
+    
   },
  ]
  return (
   <div className=" mb-14">
    <Heading text=" OUR VALUES " className=" font-sans font-bold" />
-   <p className=" text-xl sm:w-2/4 tab:w-2/3 w-full tab:mb-4 sm:px-20 font-sans px-4 mx-auto my-0">
+   <p className=" text-xl sm:w-2/4 tab:w-2/3 w-full sm:mb-8 mb-4 sm:px-20 font-sans px-4 mx-auto my-0">
     We have a set of values that guide our way of doing business. <b>WE{''}</b>{' '}
     are true to whatever we say we are.
    </p>
@@ -76,14 +74,16 @@ const IconsSection = () => {
    <div className=" grid sm:grid-cols-3 tab:grid-cols-2 grid-rows-1  sm:px-20 px-4 mb-12">
     {iconCards.map((x) => (
      <div
-      className="flex flex-col mr-0 py-8 mb-6 px-3 mx-4 hover:border-b-blue-700 hover:bg-gray-100  hover:border-b-2 hover:shadow-md  "
+      className="flex flex-col mr-0 py-8 mb-6 sm:px-8 px-4 mx-4 hover:border-b-blue-700 hover:bg-gray-100  hover:border-b-2 hover:shadow-md  "
       key={x.id}
      >
-      <img src={x.img} alt="" className=" items-center h-20 mb-4" />
-      <h6 className=" text-center text-xl mb-4 sm:mb-0 font-bold text-[#212121]">
+      <div className=" h-10 mr-0 mb-8">
+       <img src={x.img} alt="" className="" />
+      </div>
+      <h6 className=" text-xl mb-4 sm:mb-0 font-bold text-[#212121]">
        {x.heading}
       </h6>
-      <p className=" sm:py-6 sm:px-8 tab:px-0   text-lg">{x.text}</p>
+      <p className=" sm:py-4 tab:px-0   text-lg">{x.text}</p>
      </div>
     ))}
    </div>
